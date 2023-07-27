e9a6e4f7937f0bf1074e1b81f692f341bc899b2c
Asandei Stefan <asandei.stefanel@gmail.com>
2023-07-27 16:45:34 +0300

    fix bug

--- app/components/sections/app-shell.tsx
+++ app/components/sections/app-shell.tsx
@@ -19,34 +19,32 @@ export const AppShell = ({
     <div className="h-screen overflow-y-hidden">
       <div className="w-full bg-background h-14 flex flex-row items-center p-2 justify-between">
         {topBar}
-        <div className="hidden md:inline">
+        <div className="hidden md:flex flex-row gap-2">
+          <Button
+            className="hidden md:inline"
+            variant={"outline"}
+            onClick={() => {
+              const current = localStorage.getItem("mapStyle");
+              localStorage.setItem("mapStyle", current == "0" ? "1" : "0");
+              dispatch(
+                setPosition({
+                  lat: position.lat + 0.00001,
+                  lon: position.lon,
+                })
+              );
+            }}
+          >
+            Toggle style
+          </Button>
           {/* <p className="font-bold text-xl">Remix Maps</p> */}
           {pathname !== "/bucharest" ? (
             <Link to="/bucharest">
               <Button variant={"outline"}>Bucharest</Button>
             </Link>
           ) : (
-            <div className="flex flex-row gap-2">
-              <Button
-                className="hidden md:inline"
-                variant={"outline"}
-                onClick={() => {
-                  const current = localStorage.getItem("mapStyle");
-                  localStorage.setItem("mapStyle", current == "0" ? "1" : "0");
-                  dispatch(
-                    setPosition({
-                      lat: position.lat + 0.00001,
-                      lon: position.lon,
-                    })
-                  );
-                }}
-              >
-                Toggle style
-              </Button>
-              <Link to="/">
-                <Button variant={"outline"}>World</Button>
-              </Link>
-            </div>
+            <Link to="/">
+              <Button variant={"outline"}>World</Button>
+            </Link>
           )}
         </div>
       </div>
